38af4e28d061194b2a6ef9285925954611bf2908
ramesh sah <rsah3798@gmail.com>
2025-09-21 23:05:34 +0545

    final

--- AI and Machine Learning/data_science/09_nested_loops.py
+++
@@ -1,6 +0,0 @@
-colors = ['red','white','blue']
-items = ['book','pen','copy']
-for color in colors:
-    for item in items:
-        print(color,item)
-    
--- AI and Machine Learning/data_science/test.py
+++
@@ -1,6 +0,0 @@
-def helloWorld():
-  return "Hello world"
-
-# if __name__=="__main__":
-#     n = helloWorld()
-#     print(n)
